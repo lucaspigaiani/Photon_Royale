65bb2d1f6c7bfdfc7413cb26588e083fd7b83d9d
Lucas Pigaiani <lucaspigaiani@gmail.com>
2025-02-05 20:35:36 -0300

    Imported erika model, created playerController  and cameraController

---
+++ Assets/MyGame/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform followTarget;
+    [SerializeField] private float rotationSpeed = 2f;
+    [SerializeField] private float distance;
+    [SerializeField] private float minVerticalAngle = -45;
+    [SerializeField] private float maxVerticalAngle = 45;
+
+    [SerializeField] private Vector2 framingoffSet;
+
+    [SerializeField] private bool invertX;
+    [SerializeField] private bool invertY;
+
+    private float rotationX;
+    private float rotationY;
+    private float invertXVal;
+    private float invertYVal;
+
+    private void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        invertXVal = (invertX) ? -1 : 1;
+        invertYVal = (invertY) ? -1 : 1;
+
+        rotationX += Input.GetAxis("Mouse Y") * invertYVal * rotationSpeed;
+        rotationX = Mathf.Clamp(rotationX, minVerticalAngle, maxVerticalAngle);
+
+        rotationY += Input.GetAxis("Mouse X") * invertXVal * rotationSpeed;
+
+        var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
+
+        var focusPosition = followTarget.position + new Vector3(framingoffSet.x, framingoffSet.y);
+
+        transform.position = focusPosition - targetRotation * new Vector3(0, 0, distance);
+        transform.rotation = targetRotation;
+    }
+
+    public Quaternion planarRotation => Quaternion.Euler(0, rotationY, 0);
+
+}
--- Assets/MyGame/Scripts/PlayerController.cs
+++ Assets/MyGame/Scripts/PlayerController.cs
@@ -3,12 +3,44 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private CameraController cameraController;
+
+    [SerializeField] private float moveSpeed = 5f;
+    [SerializeField] private float rotationSpeed = 500f;
+
+    Quaternion targetRotation;
+    private void Update()
+    {
+        float h = Input.GetAxis("Horizontal"); 
+        float v = Input.GetAxis("Vertical");
+
+        float moveAmount = MathF.Abs(h) + MathF.Abs(v);
+
+        var moveInput = new Vector3(h, 0, v).normalized;
+        var moveDir = cameraController.planarRotation * moveInput;
+       
+        if (moveAmount > 0)
+        {
+            
+            targetRotation = Quaternion.LookRotation(moveDir);
+        }
+
+        if (moveAmount > 0.5f)
+        {
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+        }
+
+        transform.rotation =  Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+    }
+
+
+    #region Photon
     private PhotonView photonView;
 
-     
     public void KickPlayer(Player player)
     {
         
@@ -31,4 +63,6 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Você foi expulso da sala!");
         PhotonNetwork.LeaveRoom(); // Força o jogador a sair
     }
+
+    #endregion
 }
